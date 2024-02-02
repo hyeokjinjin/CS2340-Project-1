4da4450e1a4ed35a19ee7a0d435fa09abd75493c
LukeHartzell1 <123661552+LukeHartzell1@users.noreply.github.com>
2024-02-02 16:47:22 -0500

    implemented to do list

--- app/src/main/java/com/example/project1/todoFragment.java
+++ app/src/main/java/com/example/project1/todoFragment.java
@@ -7,25 +7,126 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.project1.databinding.FragmentTodoBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.example.project1.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class todoFragment extends Fragment {
 
+public class todoFragment extends Fragment {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+    private FragmentTodoBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_todo, container, false);
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.todo_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding = FragmentTodoBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        // sets up list view
+        lvItems = view.findViewById(R.id.lv_to_do);
+        items = new ArrayList<>();
+        readItems(); // read cached items
+        itemsAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        // Call listener for long tap and hold for removing item
+        setupListViewListener();
+
+        // Set up the click listener for the "Add" button
+        binding.buttonAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Todo with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                onAddItem(v);
             }
         });
 
+
         return view;
     }
+//        View view = inflater.inflate(R.layout.fragment_todo, container, false);
+//
+//        binding = FragmentTodoBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        //sets up list view
+//        lvItems = (ListView) findViewById(R.id.lv_to_do);
+//        items = new ArrayList<String>();
+//        readItems(); //read cached items
+//        itemsAdapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1, items);
+//        lvItems.setAdapter(itemsAdapter);
+//
+//        //Call listener for long tap and hold for removing item
+//        setupListViewListener();
+//
+//        return view;
+//    }
+
+    //Next two blocks of code cache the data by reading and writing to/from memory
+// Next two blocks of code cache the data by reading and writing to/from memory
+    private void readItems() {
+        File filesDir = requireContext().getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = requireContext().getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Attaches a long click listener to the listview with purpose of removing
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems(); //removes from stored data
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+                });
+    }
+
+    //Updates listview to add item
+    public void onAddItem(View v) {
+        EditText etNewItem = binding.etAddItem;
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems(); // adds to stored data
+    }
+
 }
